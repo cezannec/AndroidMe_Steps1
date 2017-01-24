9daab87a7853b988a1ce720d2f914b1640993dc2
cezannec <cezanne.camacho@gmail.com>
2017-01-24 15:32:16 -0800

    Create and populate initial three body part fragments

---
+++ app/src/main/java/com/example/android/androidme_steps1/BodyPartFragment.java
@@ -0,0 +1,61 @@
+package com.example.android.androidme_steps1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by cezannec on 1/24/17.
+ */
+
+// Fragment that holds an image for an AndroidMe body part (head, body, or legs)
+    // The image it holds is determined by the image id, which is set by a call to setId()
+public class BodyPartFragment extends Fragment {
+
+    // Keep track of image resource with class variables
+    public static final String IMG_ID = "IMG_ID";
+    private int mImageId;
+
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public BodyPartFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Load current image resource id if it exists
+        if (savedInstanceState != null) {
+            mImageId = savedInstanceState.getInt(IMG_ID);
+        }
+
+        // Inflate the Android-Me image segment, populating it with an image based on it's resource id
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.image);
+
+        // Set the currently selected image resource id
+        imageView.setImageResource(mImageId);
+
+        // Return the root view
+        return rootView;
+    }
+
+
+    // Save current image resource id
+    @Override
+    public void onSaveInstanceState(Bundle currentState) {
+        currentState.putInt(IMG_ID, mImageId);
+    }
+
+
+    // This is a "setter" method that gives us a way to update the id from the adapter class
+    public void setId(int id) {
+        this.mImageId = id;
+    }
+}
--- app/src/main/java/com/example/android/androidme_steps1/CustomAndroidActivity.java
+++ app/src/main/java/com/example/android/androidme_steps1/CustomAndroidActivity.java
@@ -3,11 +3,48 @@ package com.example.android.androidme_steps1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.androidme_steps1.data.AndroidImageAssets;
+
 public class CustomAndroidActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_android);
+
+        // savedInstanceState is non-null when there is fragment state
+        // saved from previous configurations of this activity
+        // (e.g. when rotating the screen from portrait to landscape).
+        // In this case, the fragment will automatically be re-added
+        // to its container so we don't need to manually add it.
+        // For more information, see the Fragments API guide at:
+        //
+        // http://developer.android.com/guide/components/fragments.html
+
+
+        if (savedInstanceState == null) {
+            // Create the head, body, and leg fragments and add it to the activity
+            // using a fragment transaction.
+
+            // Default is first in list
+
+            BodyPartFragment headFragment = new BodyPartFragment();
+            headFragment.setId(AndroidImageAssets.getHeads().get(0));
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.headFragment, headFragment)
+                    .commit();
+
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setId(AndroidImageAssets.getBods().get(0));
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.bodyFragment, bodyFragment)
+                    .commit();
+
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setId(AndroidImageAssets.getLegs().get(0));
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.legFragment, legFragment)
+                    .commit();
+        }
     }
 }
