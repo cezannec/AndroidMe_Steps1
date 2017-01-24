b2308ab66c53a41333a945825cb9298ddef10c16
cezannec <cezanne.camacho@gmail.com>
2017-01-24 14:24:29 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/androidme_steps1/CustomAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.androidme_steps1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomAndroidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_android);
+    }
+}
